7cd0d90676d2e68a54f50b17c2f7159e9867b6a1
wagner marques <wagenrfeitosa2000@hotmail.com>
2024-06-21 08:42:03 -0300

    User insert

--- src/main/java/com/aula_23/secao_23_projeto_web_spring/resources/UseResource.java
+++ src/main/java/com/aula_23/secao_23_projeto_web_spring/resources/UseResource.java
@@ -1,13 +1,17 @@
 package com.aula_23.secao_23_projeto_web_spring.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.aula_23.secao_23_projeto_web_spring.entities.User;
 import com.aula_23.secao_23_projeto_web_spring.services.UserService;
@@ -47,6 +51,23 @@ public class UseResource {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
-	
+	/*@RequestBody Serve para dizer que irá receber uma requisição no formato Json
+	 * e será serializado para objeto User a parti do metodo post do HTTP*/
+	@PostMapping        
+	public ResponseEntity<User> insert(@RequestBody User obj){
+		obj = service.insert(obj);
+		/*OBS:Quando inserir um recurso é mais adequado retornar codigo de resposta 201 
+		 * que significa que foi criado um novo recurso para isso ->
+		 * return ResponseEntity.created(uri) recebe um objeto do tipo URI
+		 * que retorna endereco do novo recurso inserido 
+		 * Objeto URI cria um endereco para retornar 
+		 * metodo path("/{id}") indica a url será digitadono navegador ursers/id
+		 * metodo buildAndExpand(obj.getId) recebe o id que foi inserido no caso id do obj inserido
+		 * metodo toUri() converte para objeto tipo URI*/
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+		
+	}
 
 }
